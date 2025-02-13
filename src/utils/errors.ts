fd19e013c3091ad2ef29939568c67dea33246157
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2025-02-13 09:02:55 +0000

    chore: remove sentry error reporting

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -15,37 +15,6 @@ export const errorReporting = process.env.DISABLE_SENTRY !== "true" &&
   (ls === null || ls === "true");
 export const messageOnErrorReport = localStorage.getItem("settings.messageOnErrorReport") === "true";
 
-Sentry.init({
-  dsn: errorReporting
-    ? "https://51a018424102449b88f94c795cf62bb7@sentry.lemmmy.pw/2"
-    : undefined,
-  release: "kristweb2-react@" + gitVersion,
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Disable Sentry error reporting if the setting is disabled:
-  tracesSampleRate: errorReporting ? 0.2 : 0,
-
-  beforeSend(event) {
-    // Don't send an error event if error reporting is disabled
-    if (!errorReporting) return null;
-
-    // Show a message on report if the setting is enabled
-    if (messageOnErrorReport) {
-      // TODO: Find a way to translate this, while still ensuring that this file
-      //       is imported before everything else.
-      message.info("An error was automatically reported. See console for details.");
-    }
-
-    return event;
-  },
-
-  beforeBreadcrumb(breadcrumb) {
-    // Don't send a breadcrumb event if error reporting is disabled
-    if (!errorReporting) return null;
-    return breadcrumb;
-  }
-});
-
 export function criticalError(
   err: Error | string,
   captureContext?: CaptureContext
